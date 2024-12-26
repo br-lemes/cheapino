7e96ae10b3d0d803e290b5e2ed54a200a302a750
Breno Ramalho Lemes <breno@br-lemes.net>
2024-12-26 13:12:11 -0400

    fix: wasn't necessary to fix setrgb (just config)
    Better say just don't misconfig.

--- cheapino.c
+++ cheapino.c
@@ -1,26 +1,22 @@
 #include "wait.h"
 #include "quantum.h"
 
-void fix_setrgb(uint8_t r, uint8_t g, uint8_t b) {
-    rgblight_setrgb(g, r, b);
-}
-
 uint32_t flash_led(uint32_t next_trigger_time, void *cb_arg) {
     static uint8_t _color_index = 3;
     switch (_color_index++) {
-        case  0: fix_setrgb(  0, 255,   0); break;
-        case  1: fix_setrgb(  0, 255, 128); break;
-        case  2: fix_setrgb(  0, 255, 255); break;
-        case  3: fix_setrgb(  0, 128, 255); break;
-        case  4: fix_setrgb(  0,   0, 255); break;
-        case  5: fix_setrgb(127,   0, 255); break;
-        case  6: fix_setrgb(255,   0, 255); break;
-        case  7: fix_setrgb(255,   0, 127); break;
-        case  8: fix_setrgb(255,   0,   0); break;
-        case  9: fix_setrgb(255, 128,   0); break;
-        case 10: fix_setrgb(255, 255,   0); break;
-        case 11: fix_setrgb(128, 255,   0); break;
-        default: fix_setrgb(0, 0, 0); return 0;
+        case  0: rgblight_setrgb(  0, 255,   0); break;
+        case  1: rgblight_setrgb(  0, 255, 128); break;
+        case  2: rgblight_setrgb(  0, 255, 255); break;
+        case  3: rgblight_setrgb(  0, 128, 255); break;
+        case  4: rgblight_setrgb(  0,   0, 255); break;
+        case  5: rgblight_setrgb(127,   0, 255); break;
+        case  6: rgblight_setrgb(255,   0, 255); break;
+        case  7: rgblight_setrgb(255,   0, 127); break;
+        case  8: rgblight_setrgb(255,   0,   0); break;
+        case  9: rgblight_setrgb(255, 128,   0); break;
+        case 10: rgblight_setrgb(255, 255,   0); break;
+        case 11: rgblight_setrgb(128, 255,   0); break;
+        default: rgblight_setrgb(0, 0, 0); return 0;
     }
     return 100;
 }
@@ -31,10 +27,10 @@ void keyboard_post_init_user(void) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch (get_highest_layer(state)) {
-        case 1: fix_setrgb(0, 0, 255); break;
-        case 2: fix_setrgb(0, 255, 0); break;
-        case 3: fix_setrgb(255, 0, 0); break;
-        default: fix_setrgb(0, 0, 0); break;
+        case 1: rgblight_setrgb(0, 0, 255); break;
+        case 2: rgblight_setrgb(0, 255, 0); break;
+        case 3: rgblight_setrgb(255, 0, 0); break;
+        default: rgblight_setrgb(0, 0, 0); break;
     }
     return state;
 }
